611238b4876d90de0922ff5ce579e4d152e43f63
Naman <namangupta2132@gmail.com>
2026-05-08 12:16:28 +0530

    Added complete ForecastIQ project

--- app.py
+++ app.py
@@ -6,31 +6,13 @@ import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 
-# ==========================================
-# CREATE FASTAPI APP
-# ==========================================
-
 app = FastAPI(
     title="Sales Forecasting API",
     description="LSTM Based Time Series Forecasting API",
     version="1.0"
 )
-
-# ==========================================
-# LOAD MODEL
-# ==========================================
-
 model = load_model('best_lstm_sales_forecast.keras')
-
-# ==========================================
-# LOAD DATA
-# ==========================================
-
 df = pd.read_csv("data/processed_sales_data.csv")
-# ==========================================
-# PREPROCESS DATA
-# ==========================================
-
 df['Date'] = pd.to_datetime(
     df['Date'],
     dayfirst=True,
@@ -39,19 +21,11 @@ df['Date'] = pd.to_datetime(
 
 df = df.sort_values(['State', 'Date'])
 
-# ==========================================
-# SCALER
-# ==========================================
-
 scaler = MinMaxScaler()
 
 data = df['Total'].values.reshape(-1,1)
 
 scaled_data = scaler.fit_transform(data)
-
-# ==========================================
-# HOME ROUTE
-# ==========================================
 
 @app.get("/")
 def home():
@@ -60,26 +34,13 @@ def home():
         "message": "Sales Forecasting API is running"
     }
 
-# ==========================================
-# FORECAST ROUTE
-# ==========================================
-
 @app.get("/forecast")
 def forecast_sales():
-
-    # --------------------------------------
-    # LAST 30 VALUES
-    # --------------------------------------
-
     last_sequence = scaled_data[-30:]
 
     future_predictions = []
 
     current_sequence = last_sequence.copy()
-
-    # --------------------------------------
-    # PREDICT NEXT 56 DAYS
-    # --------------------------------------
 
     for _ in range(56):
 
@@ -94,11 +55,6 @@ def forecast_sales():
             current_sequence[1:],
             pred
         )
-
-    # --------------------------------------
-    # INVERSE TRANSFORM
-    # --------------------------------------
-
     future_predictions = np.array(
         future_predictions
     ).reshape(-1,1)
@@ -106,30 +62,15 @@ def forecast_sales():
     future_predictions = scaler.inverse_transform(
         future_predictions
     )
-
-    # --------------------------------------
-    # REMOVE NEGATIVE VALUES
-    # --------------------------------------
-
     future_predictions = np.maximum(
         future_predictions,
         0
     )
-
-    # --------------------------------------
-    # FUTURE DATES
-    # --------------------------------------
-
     future_dates = pd.date_range(
         start=df['Date'].max() + pd.Timedelta(days=1),
         periods=56,
         freq='D'
     )
-
-    # --------------------------------------
-    # CREATE RESPONSE
-    # --------------------------------------
-
     results = []
 
     for date, prediction in zip(
